95ab2e78e77327c4cef18b597c5580d0d99f8dda
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2021-05-01 11:28:06 -0500

    ss
    sss

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Saludo from './components/Saludo'
+import Comentario from './components/Comentario';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Proyecto desde cero</h1>
+      <Saludo persona='Ignacio' edad={30} />
+      <Saludo persona='Juanito' edad={25} />
+      <Saludo persona='Pedrito' edad={27} />
+      <hr />
+      <h3>Cajita de comentarios</h3>
+      <Comentario
+        urlImagen='https://picsum.photos/64'
+        persona='Ignacio'
+        texto='ffffffffffffffffffffffffffff'
+      />
+      <Comentario
+        urlImagen='https://picsum.photos/64'
+        persona='Juanito'
+        texto='ddddddddddddddddddddddddddddd'
+      />
+      <Comentario
+        urlImagen='https://picsum.photos/64'
+        persona='Pedrito'
+        texto='wwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
+      />
+    </div>
+  );
+}
+
+export default App;
